0ef9759e8aaba6dca8f6d883487434d9f543e56c
grigoriliev <grigor@grigoriliev.com>
2023-02-23 08:31:38 +0200

    juife: use maven to build project

---
+++ src/main/java/android/app/Activity.java
@@ -0,0 +1,6 @@
+package android.app;
+
+/** A dummy class to allow successful build, which is excluded from distribution. */
+public class Activity {
+	public final void runOnUiThread (Runnable action) {}
+}
